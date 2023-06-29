d96515670601ab92f2de4f82c857f7bb1d6768a3
XLG123 <xiaolin.guan123@gmail.com>
2023-06-29 16:32:45 -0400

    initial layout of the webpage

--- src/scripts/testingExample.js
+++ src/scripts/testingExample.js
@@ -1,13 +1,13 @@
-class Example {
-  constructor(ele) {
-    this.ele = ele;
-    this.ele.innerHTML = "<h1>It’s ALIVE!!!</h1>";
-    this.handleClick = this.handleClick.bind(this);
-    this.ele.addEventListener("click", this.handleClick);
-  }
+// class Example {
+//   constructor(ele) {
+//     this.ele = ele;
+//     this.ele.innerHTML = "<h1>It’s ALIVE!!!</h1>";
+//     this.handleClick = this.handleClick.bind(this);
+//     this.ele.addEventListener("click", this.handleClick);
+//   }
 
-  handleClick() {
-    this.ele.children[0].innerText = "Ouch!";
-  }
-}
-export default Example;
+//   handleClick() {
+//     this.ele.children[0].innerText = "Ouch!";
+//   }
+// }
+// export default Example;
